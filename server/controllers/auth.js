e22be89ce5bbd799b57daabbc5843fb385351849
AJ Hephner <ajhephner178@gmail.com>
2017-09-28 10:11:42 -0400

    server through 9 signup signin work

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,55 +1,46 @@
-//handle sign in and out
 var User = require('../models/user');
-var jwt=require('jwt-simple');
-var config = require('../config');
-
-
-//issuing a JSON webtoken below
-//iat (issued at) use basic js date method to get time with timestamp variable to pass in as the vaule for iat prop
-//sub (subject) makes user.id a principle this way if the user changes their email down the road they still get stored info becasue we issue the token at the creation based on user.id which never changes. 
-
+var jwt = require('jwt-simple');
+var config =require('../config');
 
 function createUserToken(user){
 	var timestamp = new Date().getTime();
 	return jwt.encode({sub: user.id, iat: timestamp}, config.secret)
 }
+
 exports.signin = function(req, res, next){
-	//User has already had their email and pw auth
-	//we just need to give them a token 
-	res.send({token: createUserToken(req.user) });
+	
+	res.send({ token: createUserToken(req.user) });
+
 }
 exports.signup = function(req, res, next){
-	//1grab incomming request using two variables email/password
-	var email = req.body.email;
-	var password = req.body. password;
+	//test
+	var password= req.body.password;
+	var email = req.body.email; 
+	var name = req.body.name;
 
-	if(!email || !password){
+	if( !email || !password){
 		return res.status(418).send({error: 'You must provide email and pw.'});
 	}
-	//2 (check if a user with this email exists)findOne is going to go through the database and see if this users has an email there
-	//(err, existingUser) is a call back function will run after database is seached if there was an error because user already exists
-	User.findOne({email: email}, function(err, existingUser){
-			if(err){
-				return next(err);
-			}//handle search error
+	
+	User.findOne({ email: email }, function(err, existingUser){
+				if(err){
+					return next(err);
+				}
 
-			if(existingUser){
-				// return res.status(418).send(err);
-			 return res.status(418).send("email in use");
-			}//handles existing users
-			
-			//3 Create a user if that user doesn"t exist already
-			var user = new User({
-				email: email,
-				password: password
-			});
+				if(existingUser){
+					return	res.status(418).send("email in use");
+				}
 
-			//To save the record to the DB.
-			user.save(function(err){
-				if(err) {return next(err);}
-				//4 Resond to request indidcating the user was created
-				res.json({token: createUserToken(user)});
-			});
+				var user = new User({
+					email: email,
+					password: password,
+					name: name
+
+				});
+				//to save the record to the db
+				user.save(function(err){
+					if(err) {return next (err); }
+					res.json({token: createUserToken(user)});
+				});
 	});
-
 }
--- server/router.js
+++ server/router.js
@@ -1,12 +1,18 @@
 var Auth = require('./controllers/auth');
-var User = require('./models/user');
+var passportService = require('./services/passport');
+var passport = require('passport');
 
+var requireAuth = passport.authenticate('jwt', {session: false});
+var requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app){
-	app.get('/', function(req, res, next){
-		res.send("HELLO HOMEPAGE");		
+	
+	app.get('/', requireAuth, function(req, res){
+		res.send('Hello Homepage');
+		//res.send({hi:’there’});
 	});
 
 	app.post('/signup', Auth.signup);
+	app.post('/signin', requireSignin, Auth.signin);
 
 }
